2da6db1664c527ef52cf9fb3ef06f68cd51d5057
Ar4er <ar4er25@mail.ru>
2017-07-25 20:25:21 +0300

    коментарий из окна редактора

---
+++ app/src/main/java/com/bignerdrunch/android/suspendseilingcalculator/SlideAnimHelper/ItemTouchHelperViewHolder.java
@@ -0,0 +1,13 @@
+package com.bignerdrunch.android.suspendseilingcalculator.SlideAnimHelper;
+
+/**
+ * Created by ar4er25 on 4/3/2017.
+ */
+
+public interface ItemTouchHelperViewHolder {
+
+
+    void onItemSelected();
+
+    void onItemClear();
+}
---
+++ app/src/main/java/com/bignerdrunch/android/suspendseilingcalculator/SlideAnimHelper/OnStartDragListener.java
@@ -0,0 +1,13 @@
+package com.bignerdrunch.android.suspendseilingcalculator.SlideAnimHelper;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by ar4er25 on 4/3/2017.
+ */
+
+public interface OnStartDragListener {
+
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+
+}
